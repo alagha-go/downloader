1ad48ec19955de9e2a9bf27b1a9045a95c76e702
alagha-go <abdihakimosman1480@gmail.com>
2022-05-28 18:06:09 +0000

    created function to collect tvshow's elements

--- lib/crawler/tvshows/tvshow.go
+++ lib/crawler/tvshows/tvshow.go
@@ -1,6 +1,26 @@
 package tvshows
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"github.com/gocolly/colly"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+
+
+func (TvShow *TvShow)SetElements(element *colly.HTMLElement) {
+    functions := []func(element *colly.HTMLElement){}
+    functions = append(functions,  TvShow.SetReleased)
+    functions = append(functions,  TvShow.SetGenre)
+    functions = append(functions,  TvShow.SetCasts)
+    functions = append(functions,  TvShow.SetDuration)
+    functions = append(functions,  TvShow.SetCountries)
+    functions = append(functions,  TvShow.SetProducers)
+    element.ForEach(".row-line", func(index int, element *colly.HTMLElement){
+        functions[index](element)
+    })
+}
+
+
 
 func (TvShow *TvShow) SetTvShowID() {
     ID := primitive.NewObjectID()
