10b0b3e9bf56a2fb0c70a183f560ea343690cbb6
alagha-go <abdihakimosman1480@gmail.com>
2022-05-28 08:16:41 +0000

    created function to send http post requests

--- lib/crawler/movies/requests.go
+++ lib/crawler/movies/requests.go
@@ -1,7 +1,10 @@
 package movies
 
 import (
+	"bytes"
 	"encoding/json"
+	"io/ioutil"
+	"net/http"
 )
 
 
@@ -13,4 +16,24 @@ func UnmarshalLinkResponse(data []byte) (LinkResponse, error) {
 
 func (r *LinkResponse) Marshal() ([]byte, error) {
 	return json.Marshal(r)
+}
+
+
+/// func to send http Post Request
+func PostRequest(url string, data []byte, header bool, headers ...http.Header) ([]byte, http.Header, error) {
+	res, err := http.Post(url, "application/json", bytes.NewBuffer(data))
+	if err != nil {
+		return []byte(""), nil, err
+	}
+
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return []byte(""), nil, err
+	}
+
+	if !header {
+		return body, nil, nil
+	}
+	return body, res.Header, nil
 }
