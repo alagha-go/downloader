c733fb95f96a718f0833ea8880c247a708c1f769
alagha-go <abdihakimosman1480@gmail.com>
2022-05-28 07:54:35 +0000

    collects movies from all pages

---
+++ lib/crawler/movies/pages.go
@@ -0,0 +1,48 @@
+package movies
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"strconv"
+	"strings"
+
+	"github.com/gocolly/colly"
+)
+
+
+func CollectPages(PagesLength int) {
+	for index:=1; index<PagesLength+1; index++ {
+		CollectPageMovies(index)
+	}
+	SavePagesData()
+	PrintGreen("done collecting all the pages data")
+}
+
+
+func CollectPageMovies(page int) {
+	url := "https://tinyzonetv.to/movie?page=" + strconv.Itoa(page)
+	collector := colly.NewCollector()
+
+	collector.OnHTML(".film_list-wrap", CollectMovies)
+
+	collector.Visit(url)
+}
+
+
+func CollectMovies(element *colly.HTMLElement) {
+	element.ForEach(".flw-item", func(_ int, element *colly.HTMLElement) {
+		var Movie Movie
+        Movie.Title = element.ChildAttr("a", "title")
+        Movie.ImageUrl = element.ChildAttr("img", "data-src")
+        Movie.PageUrl = "https://tinyzonetv.to" + element.ChildAttr("a", "href")
+		index := strings.Index(Movie.PageUrl, "free-")
+    	Movie.Code = Movie.PageUrl[index+5:]
+		Movies = append(Movies, Movie)
+	})
+}
+
+
+func SavePagesData() {
+	data := JsonMarshal(Movies)
+	ioutil.WriteFile("./DB/Movies/movies.json", data, 0755)
+}
