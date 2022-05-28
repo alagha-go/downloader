4403dc4cbc1352167b6230e8287be78bb9568a49
alagha-go <abdihakimosman1480@gmail.com>
2022-05-28 19:48:31 +0000

    created function to find new Episodes

--- lib/crawler/tvshows/update.go
+++ lib/crawler/tvshows/update.go
@@ -1,7 +1,6 @@
 package tvshows
 
 
-
 func (TvShow *TvShow) FindNewSeasons(Tvshow *TvShow) []Season {
 	var Seasons []Season
 	for index := range TvShow.Seasons {
@@ -14,6 +13,33 @@ func (TvShow *TvShow) FindNewSeasons(Tvshow *TvShow) []Season {
 		}
 		if !exists {
 			Seasons = append(Seasons, TvShow.Seasons[index])
+		}
+	}
+	return Seasons
+}
+
+
+func (TvShow *TvShow) FindNewEpisodes(Tvshow *TvShow) []Season {
+	var Seasons []Season
+	for index := range TvShow.Seasons {
+		var NewSeason Season
+		Season := Tvshow.FindSeason(TvShow.Seasons[index].Code)
+		NewSeason = Season
+		NewSeason.Episodes = []Episode{}
+		for Eindex := range TvShow.Seasons[index].Episodes {
+			exists := false
+			for Eindex1 := range Season.Episodes {
+				if TvShow.Seasons[index].Episodes[Eindex].Code == Season.Episodes[Eindex1].Code {
+					exists = true
+					break
+				}
+			}
+			if !exists {
+				NewSeason.Episodes = append(NewSeason.Episodes, TvShow.Seasons[index].Episodes[Eindex])
+			}
+		}
+		if len(NewSeason.Episodes) > 0 {
+			Seasons = append(Seasons, NewSeason)
 		}
 	}
 	return Seasons
