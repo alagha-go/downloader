59557395144794d13b220fc061facf73a29f76a7
alagha-go <abdihakimosman1480@gmail.com>
2022-05-28 08:26:31 +0000

    created Episode struct

--- lib/crawler/tvshows/structs.go
+++ lib/crawler/tvshows/structs.go
@@ -20,3 +20,11 @@ type Season struct {
     Index                                       int                                     `json:"index,omitempty" bson:"index,omitempty"`
     Code										string									`json:"code,omitempty" bson:"code,omitempty"`
 }
+
+type Episode struct {
+	ID											primitive.ObjectID						`json:"_id,omitempty" bson:"_id,omitempty"`
+	Name										string									`json:"name,omitempty" bson:"name,omitempty"`
+    Index                                       int                                     `json:"index,omitempty" bson:"index,omitempty"`
+    Available                                   bool                                    `json:"available,omitempty" bson:"available,omitempty"`
+    Code										string									`json:"code,omitempty" bson:"code,omitempty"`
+}
