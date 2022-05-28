6b1c4c828e94161338873ae343b400e2d99dbf2c
alagha-go <abdihakimosman1480@gmail.com>
2022-05-28 07:58:40 +0000

    created function to get number of pages

--- lib/crawler/movies/main.go
+++ lib/crawler/movies/main.go
@@ -4,6 +4,10 @@ import (
 	"bytes"
 	"encoding/json"
 	"log"
+	"strconv"
+	"strings"
+
+	"github.com/gocolly/colly"
 )
 
 
@@ -14,6 +18,29 @@ var (
 
 func Main() {
 
+}
+
+
+func GetNumberOfPages() int {
+	var err error
+	var numberofPages int
+	collector := colly.NewCollector()
+
+	collector.OnHTML(".pagination.pagination-lg.justify-content-center", func(element *colly.HTMLElement) {
+		element.ForEach(".page-item", func(_ int, element *colly.HTMLElement) {
+			title := element.ChildAttr("a", "title")
+			href := element.ChildAttr("a", "href")
+			if title == "Last" {
+				href = strings.ReplaceAll(href, "/movie?page=", "")
+				numberofPages, err = strconv.Atoi(href)
+				HanleError(err)
+			}
+		})
+	})
+
+	collector.Visit("https://tinyzonetv.to/movie")
+
+	return numberofPages
 }
 
 
