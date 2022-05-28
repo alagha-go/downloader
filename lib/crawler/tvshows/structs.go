384d1a374e368001b657555f4db97bde9357ee4b
alagha-go <abdihakimosman1480@gmail.com>
2022-05-28 18:03:55 +0000

    update TvShow struct

--- lib/crawler/tvshows/structs.go
+++ lib/crawler/tvshows/structs.go
@@ -1,6 +1,10 @@
 package tvshows
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 
 type TvShow struct {
@@ -11,6 +15,17 @@ type TvShow struct {
     Available                                   bool                                    `json:"available,omitempty" bson:"available,omitempty"`
     PageUrl										string									`json:"page_url,omitempty" bson:"page_url,omitempty"`
     ImageUrl									string									`json:"image_url,omitempty" bson:"image_url"`
+    Uploaded                                    bool                                    `json:"uploaded,omitempty" bson:"uploaded,omitempty"`
+    Released									*time.Time								`json:"released,omitempty" bson:"released"`
+	Collected									bool									`json:"collected,omitempty" bson:"collected,omitempty"`
+    Genres										[]string								`json:"genre,omitempty" bson:"genre,omitempty"`
+	Server										*Server									`json:"server,omitempty" bson:"server,omitempty"`
+    Servers										[]Server								`json:"servers,omitempty" bson:"servers,omitempty"`
+    Casts										[]string								`json:"casts,omitempty" bson:"casts,omitempty"`
+    Duration									time.Duration							`json:"duration,omitempty" bson:"duration,omitempty"`
+    Countries									[]string								`json:"countries,omitempty" bson:"countries,omitempty"`
+    Producers									[]string								`json:"producers,omitempty" bson:"producers,omitempty"`
+    Description									string									`json:"description,omitempty" bson:"description,omitempty"`
 	Seasons                                     []Season                                `json:"seasons,omitempty" bson:"seasons,omitempty"`
 }
 
