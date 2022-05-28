b6bf666a7cd7f779ac58f14a8a0376d978056d08
alagha-go <abdihakimosman1480@gmail.com>
2022-05-28 07:46:52 +0000

    created struct Server

--- lib/crawler/movies/structs.go
+++ lib/crawler/movies/structs.go
@@ -9,3 +9,12 @@ type Movie struct {
     PageUrl										string									`json:"page_url,omitempty" bson:"page_url,omitempty"`
     ImageUrl									string									`json:"image_url,omitempty" bson:"image_url"`
 }
+
+
+type Server struct {
+	ID											primitive.ObjectID						`json:"_id,omitempty" bson:"_id,omitempty"`
+    Name                                        string                               	`json:"name,omitempty" bson:"name,omitempty"`
+    Id                                          string                               	`json:"id,omitempty" bson:"id,omitempty"`
+    WatchID                                     string                               	`json:"watch_id,omitempty" bson:"watch_id,omitempty"`
+    Url                                         string                               	`json:"url,omitempty" bson:"url,omitempty"`
+}
