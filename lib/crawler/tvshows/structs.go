c890a6f08009c063472b33a7911bd68cba17f3ee
alagha-go <abdihakimosman1480@gmail.com>
2022-05-28 08:24:58 +0000

    created TvShow struct

---
+++ lib/crawler/tvshows/structs.go
@@ -0,0 +1,14 @@
+package tvshows
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+
+type TvShow struct {
+	ID											primitive.ObjectID						`json:"_id,omitempty" bson:"_id,omitempty"`
+	Code										string									`json:"code,omitempty" bson:"code,omitempty"`
+	Title										string									`json:"title,omitempty" bson:"title,omitempty"`
+	Type										string									`json:"type,omitempty" bson:"type,omitempty"`
+    Available                                   bool                                    `json:"available,omitempty" bson:"available,omitempty"`
+    PageUrl										string									`json:"page_url,omitempty" bson:"page_url,omitempty"`
+    ImageUrl									string									`json:"image_url,omitempty" bson:"image_url"`
+}
