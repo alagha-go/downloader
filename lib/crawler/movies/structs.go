1508ce55af195bcb570a34aef01863a55d4d0f3f
alagha-go <abdihakimosman1480@gmail.com>
2022-05-28 17:34:40 +0000

    added fields to the Movie struct

--- lib/crawler/movies/structs.go
+++ lib/crawler/movies/structs.go
@@ -1,15 +1,31 @@
 package movies
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type Movie struct {
 	ID											primitive.ObjectID						`json:"_id,omitempty" bson:"_id,omitempty"`
 	Code										string									`json:"code,omitempty" bson:"code,omitempty"`
 	Title										string									`json:"title,omitempty" bson:"title,omitempty"`
+	Type										string									`json:"type,omitempty" bson:"type,omitempty"`
+    Available                                   bool                                    `json:"available,omitempty" bson:"available,omitempty"`
     PageUrl										string									`json:"page_url,omitempty" bson:"page_url,omitempty"`
     ImageUrl									string									`json:"image_url,omitempty" bson:"image_url"`
+    Uploaded                                    bool                                    `json:"uploaded,omitempty" bson:"uploaded,omitempty"`
+    Released									*time.Time								`json:"released,omitempty" bson:"released"`
+    Urls										[]string								`json:"urls,omitempty" bson:"urls,omitempty"`
+	Collected									bool									`json:"collected,omitempty" bson:"collected,omitempty"`
+    Genres										[]string								`json:"genre,omitempty" bson:"genre,omitempty"`
 	Server										*Server									`json:"server,omitempty" bson:"server,omitempty"`
     Servers										[]Server								`json:"servers,omitempty" bson:"servers,omitempty"`
+    Casts										[]string								`json:"casts,omitempty" bson:"casts,omitempty"`
+    Duration									time.Duration							`json:"duration,omitempty" bson:"duration,omitempty"`
+    Countries									[]string								`json:"countries,omitempty" bson:"countries,omitempty"`
+    Producers									[]string								`json:"producers,omitempty" bson:"producers,omitempty"`
+    Description									string									`json:"description,omitempty" bson:"description,omitempty"`
 }
 
 
