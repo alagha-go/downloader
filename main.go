9b931777f33ad6a27c67563dfe0f728d20721065
alagha-go <abdihakimosman1480@gmail.com>
2022-05-28 09:00:29 +0000

    called tvshows crawler

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
-import "downloader/lib/crawler/movies"
+import (
+	"downloader/lib/crawler/movies"
+	"downloader/lib/crawler/tvshows"
+)
 
 
 func main() {
 	movies.Main()
+	tvshows.Main()
 }
