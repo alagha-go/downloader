931feddae7fbebc57e8f5e0ee920cc300ed49471
alagha-go <abdihakimosman1480@gmail.com>
2022-05-28 18:38:21 +0000

    created a http handler to handle request to get Statistics

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"downloader/lib/crawler/movies"
 	"downloader/lib/crawler/tvshows"
+	"encoding/json"
+	"net/http"
 )
 
 type Statistics struct {
@@ -18,4 +20,28 @@ type Statistics struct {
 func main() {
 	movies.Main()
 	tvshows.Main()
+}
+
+
+func GetStatistics(res http.ResponseWriter, req http.Request) {
+	Statistics := []Statistics{
+		{
+			Type: "Movie",
+			PagesLength: movies.TotalNumberOfPages,
+			CurrentPageNumber: movies.CurrentPageNumber,
+			CurrentPageCollectedMovies: movies.CurrentPageCollectedMovies,
+			TotalNumberOfMovies: len(movies.Movies),
+			CurrentMovie: movies.CurrentMovie,
+		},
+		{
+			Type: "Tv-Show",
+			PagesLength: tvshows.TotalNumberOfPages,
+			CurrentPageNumber: tvshows.CurrentPageNumber,
+			CurrentPageCollectedMovies: tvshows.CurrentPageCollectedMovies,
+			TotalNumberOfMovies: len(tvshows.TvShows),
+			CurrentMovie: tvshows.CurrentMovie,
+		},
+	}
+
+	json.NewEncoder(res).Encode(Statistics)
 }
