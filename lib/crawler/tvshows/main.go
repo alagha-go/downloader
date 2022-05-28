5e0cda4b2508ade7d99ca2a20931bcdf495e05c9
alagha-go <abdihakimosman1480@gmail.com>
2022-05-28 08:40:15 +0000

    created function to get number of available pages

--- lib/crawler/tvshows/main.go
+++ lib/crawler/tvshows/main.go
@@ -4,12 +4,41 @@ import (
 	"bytes"
 	"encoding/json"
 	"log"
+	"strconv"
+	"strings"
+
+	"github.com/gocolly/colly"
 )
 
 
 func Main() {
 
 }
+
+
+func GetNumberOfPages() int {
+	var err error
+	var numberofPages int
+	collector := colly.NewCollector()
+
+	collector.OnHTML(".pagination.pagination-lg.justify-content-center", func(element *colly.HTMLElement) {
+		element.ForEach(".page-item", func(_ int, element *colly.HTMLElement) {
+			title := element.ChildAttr("a", "title")
+			href := element.ChildAttr("a", "href")
+			if title == "Last" {
+				href = strings.ReplaceAll(href, "/tv-show?page=", "")
+				numberofPages, err = strconv.Atoi(href)
+				HanleError(err)
+			}
+		})
+	})
+
+	collector.Visit("https://tinyzonetv.to/tv-show")
+
+	return numberofPages
+}
+
+
 
 func JsonMarshal(data interface{}) []byte {
 	var buff bytes.Buffer
