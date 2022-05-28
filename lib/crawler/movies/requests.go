dd99ebc00e43758e4c0dc9e85aaea157a0818b7e
alagha-go <abdihakimosman1480@gmail.com>
2022-05-28 08:17:33 +0000

    created function to send http get requests

--- lib/crawler/movies/requests.go
+++ lib/crawler/movies/requests.go
@@ -37,3 +37,23 @@ func PostRequest(url string, data []byte, header bool, headers ...http.Header) (
 	}
 	return body, res.Header, nil
 }
+
+
+/// func to send http Get Request
+func GetRequest(url string, header bool, headers ...http.Header) ([]byte, http.Header, error) {
+	res, err := http.Get(url)
+	if err != nil {
+		return []byte(""), nil, err
+	}
+
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return []byte(""), nil, err
+	}
+
+	if !header {
+		return body, nil, nil
+	}
+	return body, res.Header, nil
+}
