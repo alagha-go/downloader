c9ab4c2262355487da15c2087a906ff0cf207915
alagha-go <abdihakimosman1480@gmail.com>
2022-05-28 08:15:48 +0000

    created functions to marshal and unmarshal LinkResponse struct

---
+++ lib/crawler/movies/requests.go
@@ -0,0 +1,16 @@
+package movies
+
+import (
+	"encoding/json"
+)
+
+
+func UnmarshalLinkResponse(data []byte) (LinkResponse, error) {
+	var r LinkResponse
+	err := json.Unmarshal(data, &r)
+	return r, err
+}
+
+func (r *LinkResponse) Marshal() ([]byte, error) {
+	return json.Marshal(r)
+}
