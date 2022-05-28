249d2cf5d94c47f05ac83f7fc61f314f2039d001
alagha-go <abdihakimosman1480@gmail.com>
2022-05-28 07:46:28 +0000

    created struct Movie

---
+++ lib/crawler/movies/structs.go
@@ -0,0 +1,11 @@
+package movies
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type Movie struct {
+	ID											primitive.ObjectID						`json:"_id,omitempty" bson:"_id,omitempty"`
+	Code										string									`json:"code,omitempty" bson:"code,omitempty"`
+	Title										string									`json:"title,omitempty" bson:"title,omitempty"`
+    PageUrl										string									`json:"page_url,omitempty" bson:"page_url,omitempty"`
+    ImageUrl									string									`json:"image_url,omitempty" bson:"image_url"`
+}
