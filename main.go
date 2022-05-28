95d04305ce804f0aa704365a516d02e8c107bb21
alagha-go <abdihakimosman1480@gmail.com>
2022-05-28 18:37:33 +0000

    created statistics type to get progress over http

--- main.go
+++ main.go
@@ -5,6 +5,15 @@ import (
 	"downloader/lib/crawler/tvshows"
 )
 
+type Statistics struct {
+	Type									string						`json:"type,omitempty"`
+	PagesLength								int							`json:"pages-length,omitempty"`
+	CurrentPageNumber						int							`json:"current-page,omitempty"`
+	CurrentPageCollectedMovies				int							`json:"page-collected-movies,omitempty"`
+	TotalNumberOfMovies						int							`json:"total-movies,omitempty"`
+	CurrentMovie							int							`json:"current-movie,omitempty"`
+}
+
 
 func main() {
 	movies.Main()
